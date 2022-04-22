5d44abdf819536653272eb3daa109002bfa24ebd
megan <mtpott23@gmail.com>
2022-04-22 17:42:11 -0400

    working add/show queries

--- db/schema.sql
+++ db/schema.sql
@@ -23,6 +23,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 )
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,19 @@
-INSERT INTO department (name)
-VALUES 
-('Engineering'),
-('Human Resources'),
-('Software Dev.');
+-- INSERT INTO department (name)
+-- VALUES 
+-- ('Engineering'),
+-- ('Human Resources'),
+-- ('Software Dev.');
 
--- INSERT INTO role (title, salary)
+-- INSERT INTO role (title, salary, department_id)
 -- VALUES 
 -- ('Engineer', '90000.0'),
 -- ('HR Representative', '85000.0'),
 -- ('Developer', '90000.0'),
 -- ('CEO', '100000.0');
 
--- INSERT INTO employee (first_name, last_name)
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id)
 -- VALUES
--- ('John', 'Smith'),
--- ('Jane', 'Doe'),
--- ('Kevin', 'Schmidt');
+-- ('John', 'Smith', 1, NULL),
+-- ('Jane', 'Doe', 3, 1),
+-- ('Kevin', 'Schmidt', 2, 4),
+-- ('Alice', 'Walker', 4, NULL);
